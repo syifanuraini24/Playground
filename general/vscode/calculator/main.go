c62e2884fe6fc5356fb21d6dfb84df098d48a117
Muhammad Jaysy Ansharulloh <muhammad.ansharulloh@tiket.com>
2022-03-07 20:55:55 +0700

    fix calculator error

--- general/vscode/calculator/main.go
+++ general/vscode/calculator/main.go
@@ -1,45 +1,47 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func main() {
-	var operator string
-	var number1, number2 int
+	reader := bufio.NewReader(os.Stdin)
 
-	n1 := os.Getenv("FIRST_NUMBER")
-	number1, _ = strconv.Atoi(n1)
+	fmt.Print("number 1: ")
+	n1, _ := reader.ReadString('\n')       // 10\n
+	n1 = strings.Replace(n1, "\n", "", -1) // 10
+	number1, _ := strconv.Atoi(n1)
 
-	fmt.Printf("\nFirst number: %d", number1)
+	fmt.Printf("First number: %v\n", number1)
 
-	n2 := os.Getenv("SECOND_NUMBER")
-	number2, _ = strconv.Atoi(n2)
+	fmt.Print("number 2: ")
+	n2, _ := reader.ReadString('\n')
+	n2 = strings.Replace(n2, "\n", "", -1)
+	number2, _ := strconv.Atoi(n2)
 
-	fmt.Printf("\nSecond number: %d", number2)
+	fmt.Printf("Second number: %d\n", number2)
 
-	operator = os.Getenv("ACTION")
-	fmt.Printf("\nSelected Operator (+,-,/,%%,*): %s\n", operator)
+	fmt.Print("operator: ")
+	operator, _ := reader.ReadString('\n')             // +\n
+	operator = strings.Replace(operator, "\n", "", -1) // +
+	fmt.Printf("Selected Operator (+,-,/,%%,*): %s\n", operator)
 
 	output := 0
 	switch operator {
 	case "+":
 		output = number1 + number2
-		break
 	case "-":
 		output = number1 - number2
-		break
 	case "*":
-		output = number1 + number2
-		break
+		output = number1 * number2
 	case "/":
 		output = number1 / number2
-		break
 	case "%":
-		output = number2 % number1
-		break
+		output = number1 % number2
 	default:
 		fmt.Println("Invalid Operation")
 	}
